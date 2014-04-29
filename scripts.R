f1f1746275c333c26f5940131859f06c3afc82c9
mirjalil <vmirjalily@gmail.com>
2014-04-28 23:25:09 -0400

    update to patt_rec

---
+++ scripts.R
@@ -0,0 +1,3 @@
+> svg("error.1.svg")> par(mar=c(0.7, 4.8, 1, 1))> barplot(d, cex.axis=1.8, cex.lab=1.8, col="steelblue", ylab="Classificatoin Error")
+> text(x=0.7, y=2, "True Density", cex=1.6, srt=90)> text(x=1.84, y=2, "MLE Density", cex=1.6, srt=90)> text(x=3.14, y=2.3, "Parzen Window", cex=1.6, srt=90)> text(x=4.3, y=2.3, "1-NN classification", cex=1.6, srt=90)> dev.off()
+
