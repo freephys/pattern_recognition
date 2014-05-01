6f081be55e1571917293488eeab8c814771d1824
mirjalil <vmirjalily@gmail.com>
2014-05-01 00:33:16 -0400

    update to patt_rec

--- plot_learningCurve.R
+++ plot_learningCurve.R
@@ -18,4 +18,6 @@ lines(d$V2, d$V4, col="darkgreen", pch=17, cex=2, lwd=4)
 points(d$V2, d$V5, col="darkred", pch=15, cex=2, lwd=4)
 lines(d$V2, d$V5, col="darkred", pch=15, cex=2, lwd=4)
 
+legend(x=0.2, y=9.4, col=c("steelblue", "darkgreen", "darkred"), lwd=4, pch=c(19, 17, 15), pt.cex=1.5, legend=c("MLE", "Parzen Window", "1-NN"), box.col="white", box.lwd=0, cex=1.7)
+
 dev.off()
