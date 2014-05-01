f0f7db69a02362c1fb6429fcab744d17a422f87e
mirjalil <vmirjalily@gmail.com>
2014-04-30 21:08:33 -0400

    update to patt_rec

---
+++ plot_learningCurve.R
@@ -0,0 +1,16 @@
+
+
+svg("learning_curve.1.svg")
+par(mar=c(4.9, 4.9, 0.5, 0.5))
+
+
+plot(d$V2, d$V3, col="steelblue", pch=19, xlab="Trainig size ratio", ylab="Classification error rate", cex.axis=1.8, cex.lab=1.8, ylim=c(miny, maxy), cex=2)
+lines(d$V2, d$V3, col="steelblue", pch=19, lwd=4)> points(d$V2, d$V4, col="green", pch=17, cex=2, lwd=4)
+
+points(d$V2, d$V4, col="darkgreen", pch=17, cex=2, lwd=4)
+lines(d$V2, d$V4, col="darkgreen", pch=17, cex=2, lwd=4)
+
+points(d$V2, d$V5, col="darkred", pch=15, cex=2, lwd=4)
+lines(d$V2, d$V5, col="darkred", pch=15, cex=2, lwd=4)
+
+dev.off()
